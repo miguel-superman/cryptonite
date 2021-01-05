7c91c1acbec67737af40d627b61a3b2524f8080a
Miguel Mason <miguelmasonkp@gmail.com>
2021-01-05 16:10:20 -0500

    first commit

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,61 @@
+	@extends('layouts.app')
+	@section('content')
+	<div class="container-fluid">
+		@include('layouts.nav')
+        <div class="relative flex items-top justify-center min-h-screen bg-gray-100 dark:bg-gray-900 sm:items-center sm:pt-0">
+            @if (Route::has('login'))
+                <div class="hidden fixed top-0 right-0 px-6 py-4 sm:block">
+                    @auth
+                        <a href="{{ url('/home') }}" class="text-sm text-gray-700 underline">Home</a>
+                    @else
+                        <a href="{{ route('login') }}" class="text-sm text-gray-700 underline">Login</a>
+
+                        @if (Route::has('register'))
+                            <a href="{{ route('register') }}" class="ml-4 text-sm text-gray-700 underline">Register</a>
+                        @endif
+                    @endauth
+                </div>
+            @endif
+        </div>
+		@
+
+		<div class="card">
+		<div class="card-header text-success">
+		<span>
+			<h4>
+				About our team 
+			</h4>
+		</span>
+		</div>
+		<div class="card-body">
+			<p>
+				<h5>
+				We at GORC Solutions Limited are always prepared for the job at hand .
+				</h5>
+			</p>
+		</div>
+		</div>
+
+		<div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
+			<ol class="carousel-indicators">
+				<li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
+				<li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
+				<li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
+			</ol>
+			<div class="carousel-inner">
+				<div class="carousel-item active">
+				<img src="/img/cell-site.jpg" class="d-block w-100" alt="...">
+				</div>
+				
+			</div>
+			<a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
+				<span class="carousel-control-prev-icon" aria-hidden="true"></span>
+				<span class="sr-only">Previous</span>
+			</a>
+			<a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
+				<span class="carousel-control-next-icon" aria-hidden="true"></span>
+				<span class="sr-only">Next</span>
+			</a>
+		</div>
+	</div>
+@endsection
